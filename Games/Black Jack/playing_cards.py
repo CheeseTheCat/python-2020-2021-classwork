7fc0383374c60879aa40a59238925b3ecc1a3f74
CheeseTheCat <jambhooper@gmail.com>
2020-12-17 10:18:40 -0700

    Started work on black jack

---
+++ Games/Black Jack/playing_cards.py
@@ -0,0 +1,80 @@
+# James hooper
+# 12/17/2020
+# black jack / 21 game
+import random
+########################################################################################################
+class Card(object):
+    RANKS = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+    SUITS = ["♠", "♣", "♢", "♡"]
+    def __init__(self,suit,rank):
+
+        self.suit = suit
+        self.rank = rank
+
+    def __str__(self):
+        rep = str.format("""
+        +----------+
+        | {1}{0:>2}      |
+        |          |
+        |          |
+        |          |
+        |          |
+        |          |
+        |      {0:2}{1} |
+        +----------+
+        """, self.rank, self.suit)
+        return rep
+
+class Hand(object):
+    def __init__(self):
+        self.cards = []
+    def __str__(self):
+        if self.cards:
+            rep = ""
+            for card in self.cards:
+                rep += str(card)
+        else:
+            rep = "<EMPTY>"
+        return rep
+
+    def clear(self):
+        self.cards = []
+
+    def add(self,card):
+        self.cards.append(card)
+
+    def give(self,card,other_hand):
+        self.cards.remove(card)
+        other_hand.add(card)
+
+class Deck(Hand):
+    def shuffle(self):
+        import random
+        random.shuffle(self.cards)
+
+    def populate(self):
+        for suit in Card.SUITS:
+            for rank in Card.RANKS:
+                self.add(Card(rank,suit))
+
+    def deal(self,hands,per_hand=1):
+        for rounds in range(per_hand):
+            for hand in hands:
+                if self.cards:
+                    top_card = self.cards[0]
+                    self.give(top_card,hand)
+                else:
+                    print("Out of cards clearing hands and reshuffleing")
+                    self.clear()
+                    self.populate()
+                    self.shuffle()
+                    for hand in hands:
+                        hand.clear()
+                    self.deal()
+
+
+deck = Deck()
+deck.populate()
+deck.shuffle()
+print(deck)
+print(len(deck.cards))
