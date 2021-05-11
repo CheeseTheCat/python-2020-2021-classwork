aa5ca33fb76dc3c19fc37111215a495a4cc768e2
CheeseTheCat <jambhooper@gmail.com>
2021-05-11 10:16:02 -0600

    added kingofthesky

---
+++ Games/KingOfTheSky/settings.py
@@ -0,0 +1,27 @@
+# game options/settings
+TITLE = "King OF The Sky"
+WIDTH = 640 # 20 tiles
+HEIGHT = 960 # 30 tiles
+FPS = 60
+
+# Player properties
+PLAYER_ACC = 0.07
+PLAYER_FRICTION = -0.05
+PLAYER_GRAV = 0.1
+
+# define colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+LIGHTBLUE = (0, 155, 155)
+DARKGREY = (40, 40, 40)
+LIGHTGREY = (100, 100, 100)
+
+BGCOLOR = DARKGREY
+
+TILESIZE = 32
+GRIDWIDTH = WIDTH / TILESIZE
+GRIDHEIGHT = HEIGHT / TILESIZE
---
+++ Games/KingOfTheSky/sprites.py
@@ -0,0 +1,101 @@
+# Sprite classes for game
+import pygame as pg
+from settings import *
+vec = pg.math.Vector2
+
+class Player1(pg.sprite.Sprite):
+    def __init__(self, game, x, y):
+        self.groups = game.all_sprites, game.player_group1
+        pg.sprite.Sprite.__init__(self, self.groups)
+        self.game = game
+        self.image = pg.Surface((TILESIZE, TILESIZE))
+        self.image.fill(YELLOW)
+        self.rect = self.image.get_rect()
+        self.x = x * TILESIZE
+        self.y = y * TILESIZE
+        self.pos = vec(self.x, self.y)
+        self.vel = vec(0, 0)
+        self.acc = vec(0, 0)
+
+    def jump(self):
+        self.vel.y = -3
+
+
+    def update(self):
+        self.acc = vec(0, PLAYER_GRAV)
+        if self.vel.y > 2.5:
+            self.vel.y = 2.5
+        keys = pg.key.get_pressed()
+        if keys[pg.K_a]:
+            self.acc.x = -PLAYER_ACC
+        if keys[pg.K_d]:
+            self.acc.x = PLAYER_ACC
+
+        # apply friction
+        self.acc.x += self.vel.x * PLAYER_FRICTION
+        # equations of motion
+        self.vel += self.acc
+        self.pos += self.vel + 0.5 * self.acc
+        # wrap around the sides of the screen
+        if self.pos.x > WIDTH:
+            self.pos.x = 0
+        if self.pos.x < 0:
+            self.pos.x = WIDTH
+        if self.pos.y < 0:
+            self.vel.y = 10
+        self.rect.midbottom = self.pos
+
+class Player2(pg.sprite.Sprite):
+    def __init__(self, game, x, y):
+        self.groups = game.all_sprites, game.player_group1
+        pg.sprite.Sprite.__init__(self, self.groups)
+        self.game = game
+        self.image = pg.Surface((TILESIZE, TILESIZE))
+        self.image.fill(BLUE)
+        self.rect = self.image.get_rect()
+        self.x = x * TILESIZE
+        self.y = y * TILESIZE
+        self.pos = vec(self.x, self.y)
+        self.vel = vec(0, 0)
+        self.acc = vec(0, 0)
+
+    def jump(self):
+        self.vel.y = -3
+
+
+    def update(self):
+        self.acc = vec(0, PLAYER_GRAV)
+        if self.vel.y > 2.5:
+            self.vel.y = 2.5
+        keys = pg.key.get_pressed()
+        if keys[pg.K_LEFT]:
+            self.acc.x = -PLAYER_ACC
+        if keys[pg.K_RIGHT]:
+            self.acc.x = PLAYER_ACC
+
+        # apply friction
+        self.acc.x += self.vel.x * PLAYER_FRICTION
+        # equations of motion
+        self.vel += self.acc
+        self.pos += self.vel + 0.5 * self.acc
+        # wrap around the sides of the screen
+        if self.pos.x > WIDTH:
+            self.pos.x = 0
+        if self.pos.x < 0:
+            self.pos.x = WIDTH
+        if self.pos.y < 0:
+            self.vel.y = 10
+        self.rect.midbottom = self.pos
+
+class Wall(pg.sprite.Sprite):
+    def __init__(self, game, x, y):
+        self.groups = game.all_sprites, game.walls
+        pg.sprite.Sprite.__init__(self, self.groups)
+        self.game = game
+        self.image = pg.Surface((TILESIZE, TILESIZE))
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect()
+        self.x = x
+        self.y = y
+        self.rect.x = x * TILESIZE
+        self.rect.y = y * TILESIZE
